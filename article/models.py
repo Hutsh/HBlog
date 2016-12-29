ce0c1b9c3595353b0ad49ec3877365ad635a9211
TianShuo Hu <TianShuo Hu>
2016-12-29 10:14:31 +0800

    Initial commit

---
+++ article/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model) :
+    title=models.CharField(max_length=100) #title
+    category=models.CharField(max_length=50, blank=True) #category
+    date_time=models.DateTimeField(auto_now_add=True) #time
+    content=models.TextField(blank=True, null=True) #content
+    TYPE_CHOICES=(
+        ('A','Audio'),
+        ('V','Video'),
+        ('I','Image'),
+        ('S','Status'),
+        ('B','Blog')
+    )
+    post_type=models.CharField(max_length=2,choices=TYPE_CHOICES,default='B')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-date_time']
